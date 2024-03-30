62529b70e189778f4e22fae22b524bb7475657b9
Olegkuruliak <118370221+Olegkrkl@users.noreply.github.com>
2024-03-30 14:58:11 +0200

    Create task7.py

---
+++ task7.py
@@ -0,0 +1,14 @@
+a = -5
+b = 10
+c = -3
+
+count_negative = 0
+
+if a < 0:
+    count_negative += 1
+if b < 0:
+    count_negative += 1
+if c < 0:
+    count_negative += 1
+
+print("Кількість негативних чисел серед введених:", count_negative)
