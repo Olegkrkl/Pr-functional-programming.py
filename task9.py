a906259e749a29eb79a12b63c5a62662a4c9247a
Olegkuruliak <118370221+Olegkrkl@users.noreply.github.com>
2024-03-30 14:59:28 +0200

    Create task9.py

---
+++ task9.py
@@ -0,0 +1,14 @@
+a = 5.8
+b = 10
+c = -3.7
+
+count_integers = 0
+
+if int(a) == a:
+    count_integers += 1
+if int(b) == b:
+    count_integers += 1
+if int(c) == c:
+    count_integers += 1
+
+print("Кількість цілих чисел серед введених:", count_integers)
