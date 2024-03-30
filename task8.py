3edd61709fea3d1a9df9678569b0e3e3862226e1
Olegkuruliak <118370221+Olegkrkl@users.noreply.github.com>
2024-03-30 14:58:47 +0200

    Create task8.py

---
+++ task8.py
@@ -0,0 +1,14 @@
+a = -2
+b = 5
+c = 8
+
+count_positive = 0
+
+if a > 0:
+    count_positive += 1
+if b > 0:
+    count_positive += 1
+if c > 0:
+    count_positive += 1
+
+print("Кількість додатних чисел серед введених:", count_positive)
